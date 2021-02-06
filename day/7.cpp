44e92bff687451aa94157f36cd3e08b775d41496
NeeeeT <nailazeve@gmail.com>
2021-02-06 22:59:40 +0800

    header included > practiced.

---
+++ day/7.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+#include "7.h"
+
+using namespace std;
+
+Demo::Demo(){
+    set_a(1);
+    set_b(1);
+}
+Demo::Demo(int n1){
+    set_a(n1);
+    set_b(n1);
+}
+Demo::Demo(int n1, int n2){
+    set_a(n1);
+    set_b(n2);
+}
+int Demo::doSomething(){
+    return get_a()+get_b();
+}
+void Demo::set_a(int n){
+    a = n;
+}
+void Demo::set_b(int n){
+    b = n;
+}
+int Demo::get_a(){
+    return a;
+}
+int Demo::get_b(){
+    return b;
+}
+
+int main(){
+    Demo t1;
+    Demo t2(12);
+    Demo t3(13, 24);
+
+    cout << t1.doSomething() << endl;
+    cout << t2.doSomething() << endl;
+    cout << t3.doSomething() << endl;
+
+
+    system("pause");    
+    return 0;   
+}
---
+++ day/7.h
@@ -0,0 +1,14 @@
+class Demo{
+public:
+    Demo();
+    Demo(int);
+    Demo(int,int);
+    void set_a(int);
+    void set_b(int);
+    int get_a();
+    int get_b();
+    int doSomething();
+private:
+    int a;
+    int b;
+};
